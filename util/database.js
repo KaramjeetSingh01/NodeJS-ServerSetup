9a1bfe3d319bae752e83a526160b496eba54a2b6
karamjeet.singh <karamjeetsingh01@yahoo.com>
2022-07-10 16:25:46 +0300

    commit on 'master;

---
+++ util/database.js
@@ -0,0 +1,5 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'P@ssw0rd', {dialect: 'mysql', host: 'localhost'});
+
+module.exports = sequelize;
